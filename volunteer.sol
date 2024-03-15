914ba85c694e4455b7bda99c93e29bcddacb0a81
anjaliarora1312 <123932567+anjaliarora1312@users.noreply.github.com>
2024-03-15 13:30:00 +0530

    Update volunteer.sol

--- volunteer.sol
+++ volunteer.sol
@@ -3,34 +3,39 @@ pragma solidity ^0.8.19;
 contract Project{
   
     struct Volunteer {
-        string aadharCardNumber; 
+        uint256 aadharCardNumber; 
         string name;
         uint age;
         string contactNumber;
         bool verified;
     }
-    mapping(string => string) public cleaningdate;
+    mapping(uint256 => string) public cleaningdate;
     mapping(address => Volunteer) public advolunteers ;
-    mapping(string => Volunteer) public volunteers ;
-    event VolunteerRegistered(string indexed aadharCardNumber, string name, uint age, string contactNumber);
+    mapping(uint256 => Volunteer) public volunteers ;
+    event VolunteerRegistered(uint256 aadharCardNumber, string name, uint age, string contactNumber);
 
-    modifier isverified(string memory _aadharCardNumber) {
+    modifier isverified(uint256 _aadharCardNumber) {
         require(volunteers[_aadharCardNumber].verified, "Volunteer already registered");
         _;
     }
      
-    function registerAsVolunteer(string memory _aadharCardNumber, string memory _name, uint _age, string memory _contactNumber) external{
-        //volunteers[_aadharCardNumber]=Volunteer(_aadharCardNumber,_name,_age,_contactNumber,false);
+    function registerAsVolunteer(uint256 _aadharCardNumber, string memory _name, uint _age, string memory _contactNumber) external{
+        volunteers[_aadharCardNumber]=Volunteer(_aadharCardNumber,_name,_age,_contactNumber,false);
        advolunteers[msg.sender]=Volunteer(_aadharCardNumber,_name,_age,_contactNumber,false);
      emit VolunteerRegistered(_aadharCardNumber, _name, _age, _contactNumber);
     }
-    function getVolunteerDetails(string memory _aadharCardNumber) external view returns (string memory, uint, string memory, bool) {
+    function verify(uint256 _aadharCardNumber) public 
+    {
+          require(volunteers[_aadharCardNumber].aadharCardNumber!=_aadharCardNumber,"Volunteer not exists");
+          volunteers[_aadharCardNumber].verified=true;
+    }
+    function getVolunteerDetails(uint256 _aadharCardNumber) external view returns (string memory, uint, string memory, bool) {
         Volunteer storage volunteer = volunteers[_aadharCardNumber];
         require(volunteer.verified, "Volunteer not registered");
 
         return (volunteer.name, volunteer.age, volunteer.contactNumber, volunteer.verified);
     }
-    function assigncleaningdate(string memory _aadharCardNumber, string memory _cleandate ) public
+    function assigncleaningdate(uint256 _aadharCardNumber, string memory _cleandate ) public
     {
       require(volunteers[_aadharCardNumber].verified, "Volunteer already registered");
       cleaningdate[_aadharCardNumber]=_cleandate;
