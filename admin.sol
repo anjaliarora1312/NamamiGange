40881f8c569b4239982d3286d2e7e06158e7b89c
anjaliarora1312 <123932567+anjaliarora1312@users.noreply.github.com>
2024-03-15 13:27:32 +0530

    admin.sol

--- admin.sol
+++ admin.sol
@@ -1,30 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "./volunteer.sol";
-import "./Managertask.sol";
+import "./manager.sol";
 contract AccessControl{
     address public administrator;
     Project public volun;
     CleaningDrive public manage;
      event ManagerAdded(address indexed managerAddress);
-     event VolunteerVerified(string aadharCardNumber);
+     event VolunteerVerified(uint256 aadharCardNumber);
 
      modifier onlyAdministrator() {
         require(msg.sender == administrator, "Only administrator can perform this action");
         _;
     }
-    constructor(address _volunteeraddr, address _manageaddress) {
-        administrator = msg.sender;
-        volun = Project(_volunteeraddr);
-        manage=CleaningDrive(_manageaddress);
-    }
-    function addManager(string memory _aadharCardNumber, string memory _name, uint256 _age, string memory _contactNumber, address _admanager) external onlyAdministrator {
+   
+    function addManager(uint256 _aadharCardNumber, string memory _name, uint256 _age, string memory _contactNumber, address _admanager) external  {
         //managers[_aadharCardNumber] _name,_age,_contactNumber,_admanager] = true;
         manage.setmanager(_aadharCardNumber,_name,_age,_contactNumber,_admanager);
         emit ManagerAdded(_admanager);
     }
-    function verifyVolunteer(string memory _aadharCardNumber, string memory _name, uint _age, string memory _contactNumber) external onlyAdministrator{
-        volun.registerAsVolunteer(_aadharCardNumber,_name,_age,_contactNumber);
+    function verifyVolunteer(uint256 _aadharCardNumber) external onlyAdministrator() {
+        volun.verify(_aadharCardNumber);
         emit VolunteerVerified(_aadharCardNumber);
     }
 
