659524bcb49c28f74b630a46d9bf0d42b40bcab6
anjaliarora1312 <123932567+anjaliarora1312@users.noreply.github.com>
2024-03-15 13:28:56 +0530

    Managertask.sol

--- Managertask.sol
+++ Managertask.sol
@@ -5,7 +5,7 @@ contract CleaningDrive {
 
 
     struct Manager {
-        string aadharCardNumber; 
+        uint256 aadharCardNumber; 
         string name;
         uint age;
         string contactNumber;
@@ -13,22 +13,32 @@ contract CleaningDrive {
     }
     Manager public manager;
     Project public volunter;
+     mapping(address => Manager) public managers;
     event CleaningDriveScheduled(address indexed manager, string date);
-    function setmanager(string memory _aadharCardNumber, string memory _name, uint256 _age, string memory _contactNumber, address _admanager) public
+     
+      function setmanager(uint256 _aadharCardNumber, string memory _name, uint256 _age, string memory _contactNumber, address _admanager) public
     {
         manager=Manager(_aadharCardNumber,_name,_age,_contactNumber,_admanager);
     }
+    
+     //function setmanager(uint256 _aadharCardNumber, string memory _name, uint256 _age, string memory _contactNumber, address _admanager) public {
+       // require(managers[_admanager].aadharCardNumber == 0, "Manager already exists");
+        // managers[_admanager]= Manager({
+           // aadharCardNumber: _aadharCardNumber,
+           // name: _name,
+            //age: _age,
+           // contactNumber: _contactNumber,
+            //admanager: _admanager
+        //});
+   // }
     modifier onlyManager() {
         require(msg.sender == manager.admanager, "Only the manager can call this function");
         _;
     }
-   function scheduleCleaningDrive(string memory date, string memory aadharcard) external onlyManager {
+   function scheduleCleaningDrive(string memory date, uint256 aadharcard) external onlyManager {
         volunter.assigncleaningdate(aadharcard,date);
         emit CleaningDriveScheduled(manager.admanager, date);
     }
-   constructor(address _volunteeraddr) {
-       
-        volunter = Project(_volunteeraddr);
-      
-    }
+
+  
 }
